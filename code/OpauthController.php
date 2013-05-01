a90f8a4de9d1c7212739d0bb9ed7efb68777b8e2
Will Morgan <avengex@gmail.com>
2013-05-01 16:17:25 +0100

    Squashing out some naughty stuff

--- code/OpauthController.php
+++ code/OpauthController.php
@@ -12,9 +12,14 @@ class OpauthController extends Controller {
 		$allowed_actions = array(
 			'index',
 			'callback',
+		),
+		$url_handlers = array(
+			// Some inconsistent strategies use oauth_callback
+			'oauth_callback' => 'callback',
 		);
 
 	/**
+	 * This function only catches the request to pass it straight on.
 	 * Opauth uses the last segment of the URL to identify the auth method.
 	 * In _routes.yml we enforce a $Strategy request parameter to enforce this.
 	 * Equivalent to "index.php" in the Opauth package.
@@ -22,15 +27,110 @@ class OpauthController extends Controller {
 	 */
 	public function index(SS_HTTPRequest $request) {
 		$strategy = $request->param('Strategy');
-		$opauth = OpauthAuthenticator::opauth(true);
+		// Redirects:
+		OpauthAuthenticator::opauth(true);
 	}
 
 	/**
 	 * Equivalent to "callback.php" in the Opauth package.
+	 * If there is a problem with the response, we throw an HTTP error.
 	 * When done validating, we return back to the Authenticator continue auth.
+	 * @throws SS_HTTPResponse_Exception if any validation errors
 	 */
 	public function callback(SS_HTTPRequest $request) {
+		$opauth = OpauthAuthenticator::opauth(false);
+		$response = $this->getOpauthResponse($opauth);
+
+		// Handle all Opauth validation in this handy function
+		try {
+			$this->validateOpauthResponse($opauth, $response);
+		}
+		catch(Exception $e) {
+			$this->httpError(400, $e->getMessage());
+		}
+
 		return 'callback';
+	}
+
+	/**
+	 * Returns the response from the Oauth callback.
+	 * @throws InvalidArugmentException
+	 * @return array The response
+	 */
+	protected function getOpauthResponse(Opauth $opauth) {
+		$transportMethod = $opauth->env['callback_transport'];
+		switch($transportMethod) {
+			case 'session':
+				return $this->getResponseFromSession();
+			break;
+			case 'get':
+			case 'post':
+				return $this->getResponseFromRequest($transportMethod);
+			break;
+			default:
+				throw new InvalidArgumentException('Invalid transport method: ' . $transportMethod);
+			break;
+		}
+	}
+
+	/**
+	 * Validates the Oauth response for Opauth.
+	 * @throws InvalidArgumentException
+	 */
+	protected function validateOpauthResponse($opauth, $response) {
+		if(!empty($response['error'])) {
+			throw new InvalidArgumentException((string) $response['error']);
+		}
+
+		// Required components within the response
+		$this->requireResponseComponents(
+			array('auth', 'timestamp', 'signature'),
+			$response
+		);
+
+		// More required components within the auth section...
+		$this->requireResponseComponents(
+			array('provider', 'uid'),
+			$response['auth']
+		);
+
+		$invalidReason;
+
+		if(!$opauth->validate(
+			sha1(print_r($response['auth'], true)),
+			$response['timestamp'],
+			$response['signature'],
+			$invalidReason
+		)) {
+			throw new InvalidArgumentException('Invalid auth response: ' . $invalidReason);
+		}
+	}
+
+	/**
+	 * Shorthand for quickly finding missing components and complaining about it
+	 * @throws InvalidArgumentException
+	 */
+	protected function requireResponseComponents(array $components, $response) {
+		foreach($components as $component) {
+			if(empty($response[$component])) {
+				throw new InvalidArgumentException('Required component ' . $component . ' was missing');
+			}
+		}
+	}
+
+	/**
+	 * @return array Opauth response from session
+	 */
+	protected function getResponseFromSession() {
+		return Session::get('opauth');
+	}
+
+	/**
+	 * Looks at $method (GET, POST, PUT etc) for the response.
+	 * @return array Opauth response
+	 */
+	protected function getResponseFromRequest($method) {
+		return unserialize(base64_decode($this->request->{$method.'Var'}('opauth')));
 	}
 
 	/**
